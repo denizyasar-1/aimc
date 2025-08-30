9b72666132232fb02968e3b17d0ff4b09351d13b
denizyasar-1 <denizyasar553@gmail.com>
2025-08-30 14:32:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import http from 'http';
-import socketIo from 'socket.io';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
 // Oyuncuları saklamak için harita
 const players = new Map();
@@ -78,5 +78,3 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('Sunucu http://localhost:3000 adresinde çalışıyor.');
 });
-
-
